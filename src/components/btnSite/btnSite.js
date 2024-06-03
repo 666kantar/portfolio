6c0b68332a514f3356d4e7599727d30a8c99f19e
666kantar <slava171219@gmail.com>
2024-06-02 21:33:59 -0500

    new

---
+++ src/components/btnSite/btnSite.js
@@ -0,0 +1,7 @@
+const BtnSite = ({link}) => {
+    return ( <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
+    Visit Website
+  </a> );
+}
+ 
+export default BtnSite;
---
+++ src/helpers/projectlist.js
@@ -0,0 +1,26 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01-big.jpg";
+
+import project02 from "./../img/projects/02.jpg";
+import project02Big from "./../img/projects/02-big.jpg";
+
+
+const projects = [
+	{
+		title: 'E-Commerce site on React',
+		skills: 'React, Firebase, CSS',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: 'https://github.com/666kantar/react_proj',
+		siteLink: "https://mmdreams.netlify.app/"
+	},
+	{
+		title: 'E-Commerce site on HTML',
+		img: project02,
+		imgBig: project02Big,
+		skills: 'HTML, JavaScript, CSS',
+		gitHubLink: 'https://github.com/666kantar/zaiats',
+	}
+];
+
+export {projects}
